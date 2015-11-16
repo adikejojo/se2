147847be3383cf6ac2bf7615b7acbce6d6b437ce
toast <623039975@qq.com>
2015-11-16 09:36:39 +0800

    dzm

---
+++ LMS/src/businesslogic/receiptbl/infohelper.java
@@ -0,0 +1,44 @@
+package businesslogic.receiptbl;
+
+public class infohelper {
+	void getSendReceiptInfo(){
+		
+	}
+	
+	void getAcceptReceiptInfo(){
+		
+	}
+	
+	void getCollectionReceiptInfo(){
+		
+	}
+	
+	void getDeliveryReceiptInfo(){
+		
+	}
+	
+	void getEarningReceiptInfo(){
+		
+	}
+	
+	void getloadingReceiptInfo(){
+		
+	}
+	
+	void gettransitReceiptInfo(){
+		
+	}
+	
+	void getPaymentReceiptInfo(){
+		
+	}
+	
+	void getImportReceiptInfo(){
+		
+	}
+	
+	void getExportReceiptInfo(){
+		
+	}
+
+}
